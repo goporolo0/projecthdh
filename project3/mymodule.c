1dbf03e27fd7c1407269e8f8b225e1fd26cca9c3
TranHuuMinhNhat <81601321+TranHuuMinhNhat@users.noreply.github.com>
2022-04-14 01:50:56 +0700

    Update mymodule.c

--- project3/mymodule.c
+++ project3/mymodule.c
@@ -13,70 +13,70 @@
 #include <linux/random.h> 
 
 #define MAX_DEV 1   
+#define MAX_LEN 256     
 
-#define MAX_LEN 256     // Do lon buffer
 
-// Con tro lop device moi
 static struct class *chardev_class = NULL;
-// mang chua thong tin cac device moi loai major
+
 static struct cdev myChardev[MAX_DEV];
-// loai device - major
 static int myMajor = 0;
 
 
-// Ham thao tac voi device file
 static int myOpen(struct inode *inode, struct file *file);
 static int myRelease(struct inode *inode, struct file *file);
 static ssize_t myRead(struct file *file, char __user *buf, size_t len, loff_t *offset);
 
-// Struct thao tac voi device file
-static const struct file_operations pugs_fops = {
+
+static const struct file_operations pugs_fops = 
+{
     .owner      = THIS_MODULE,
     .open       = myOpen,
     .release    = myRelease,
     .read       = myRead
 };
 
-// Thao tac mo device file
-static int myOpen(struct inode *inode, struct file *file) {
+
+static int myOpen(struct inode *inode, struct file *file) 
+{
     printk("My Character Device >> Open\n");
     return 0;
 }
 
-// Thao tac dong device file
-static int myRelease(struct inode *inode, struct file *file) {
+static int myRelease(struct inode *inode, struct file *file)
+{
     printk("My Character Device >> Release\n");
     return 0;
 }
 
-// Ham tao so ngau nhien
-static int randomFunc(void) {
+static int randomFunc(void) 
+{
     short randNum;
     get_random_bytes(&randNum, sizeof(randNum)); 
     return randNum; 
 }
 
-// Thao tac doc device file
-static ssize_t myRead(struct file *file, char __user *buf, size_t len, loff_t *offset) {
+
+static ssize_t myRead(struct file *file, char __user *buf, size_t len, loff_t *offset)
+{
     char data[MAX_LEN] = {0};
     
     ssize_t bytes = len < (MAX_LEN-(*offset)) ? len : (MAX_LEN-(*offset));
     
     int randNum = randomFunc();
-    
     sprintf(data, "%d \n", randNum);
 
-    if (copy_to_user(buf, &data, bytes)) {
+    if (copy_to_user(buf, &data, bytes)) 
+    {
         return -EFAULT;
     }
-   
     (*offset) += bytes;
 
     return bytes;
 }
 
-// Ham khoi tao device 
-static int __init init_func(void) {
+
+static int __init init_func(void)
+{
     int i;
     dev_t myDeviceType;  
 
@@ -92,11 +92,13 @@ static int __init init_func(void) {
     
     myMajor = MAJOR(myDeviceType);
 
-    for (i = 0; i < MAX_DEV; i++) {
+    for (i = 0; i < MAX_DEV; i++)
+    {
         cdev_init(&myChardev[i], &pugs_fops);
         myChardev[i].owner = THIS_MODULE;
 
-        if(cdev_add(&myChardev[i], MKDEV(myMajor, i), 1)){
+        if(cdev_add(&myChardev[i], MKDEV(myMajor, i), 1))
+        {
             device_destroy(chardev_class, myDeviceType);
             class_destroy(chardev_class);
             unregister_chrdev_region(myDeviceType, MINORMASK);
@@ -115,29 +117,28 @@ static int __init init_func(void) {
     return 0;
 }
 
-// Ham xoa device driver
-static void __exit exit_func(void) {
+
+static void __exit exit_func(void) 
+{
     int i;
-    
     cdev_del(myChardev);
 
-    for (i = 0; i < MAX_DEV; i++) {
+    for (i = 0; i < MAX_DEV; i++)
+    {
         device_destroy(chardev_class, MKDEV(myMajor, i));
     }
-    
     class_destroy(chardev_class);
-    
     unregister_chrdev_region(MKDEV(myMajor, 0), MINORMASK);
     
     printk("Removing...");
 }
 
-// Thong tin module
+
 MODULE_LICENSE("GPL"); 
 MODULE_AUTHOR("goporo");
-MODULE_DESCRIPTION("Module tao so ngau nhien");         // mo ta chuc nang cua module 
-MODULE_SUPPORTED_DEVICE("character device");            // kieu device ma module ho tro 
+MODULE_DESCRIPTION("Module tao so ngau nhien");         
+MODULE_SUPPORTED_DEVICE("character device");            
 
-// Ham callback
+
 module_init(init_func);
 module_exit(exit_func); 
