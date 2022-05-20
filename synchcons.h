c668d3cf20bda3c7a2da0589a6964325ae754d53
phuoc3004 <100799248+phuoc3004@users.noreply.github.com>
2022-05-20 09:42:26 +0700

    Update synchcons.h

--- synchcons.h
+++ synchcons.h
@@ -1,19 +1,3 @@
-// CAE - MULTI - SYNCHCONSOLE DEFINITION
-
-#include "console.h"
-
-class SynchConsole {
-public:
-	SynchConsole();				// A SynchConsole Constructor
-	SynchConsole(char *in, char *out);	// Same with fn pointers
-	~SynchConsole();			// Delete a console instance
-
-	int Read(char *into, int numBytes);	// Read synch line
-						// Ends in EOLN or ^A
-	int Write(char *from, int numBytes);	// Write a synchronous line
-private:
-	Console		*cons;			// Pointer to an async console
-} ;
-
-// CAE - MULTI - END SECTION
-
+int i = 1;
+while (i>0) i++;
+count<<i;
